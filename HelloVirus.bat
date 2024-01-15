654906da9d35aea472cbb81fad8725bd05e04999
Thang Nguyen <118606753+VThang51@users.noreply.github.com>
2024-01-16 00:13:42 +0700

    Update HelloVirus.bat

--- HelloVirus.bat
+++ HelloVirus.bat
@@ -10,21 +10,40 @@ echo "##  | |_| |  _| | |   | |   _____| | | |  ##"
 echo "##  |  _  | |___| |___| |__|_____| |_| |  ##"
 echo "##  |_| |_|___ _|_ ___|_____| __  \___/_  ##"
 echo "##            | | / /  _/ _ \/ / / / __/  ##"
-echo "##    BATCH   | |/ // // , _/ /_/ /\ \    ##"
-echo "##    VIRUS   |___/___/_/|_|\____/___/    ##"
+echo "##            | |/ // // , _/ /_/ /\ \    ##"
+echo "##            |___/___/_/|_|\____/___/    ##"
 echo "##                                        ##"
 echo "############################################"
 set passcode=
 set /p passcode=Passcode: 
-if '%passcode%'=='DestroyMe' goto 1
+if '%passcode%'=='DestroyMe' goto destroyme
+if '%passcode%'=='SpecialDestroy' goto specialdestroy
 if '%passcode%'=='Help' goto help
-echo Wrong Passcode
+if '%passcode%'=='About' goto about
+echo [Wrong Passcode]
 pause
 goto main
-:1
-start C:\Users\%username%\Desktop\HelloVirus\HelloVirus.bat
+
+:destroyme
+echo @echo off >> C:\Users\%username%\Desktop\HelloVirus\test1.bat
+echo :main >> C:\Users\%username%\Desktop\HelloVirus\test1.bat
+echo echo Starting DestroyMe... >> C:\Users\%username%\Desktop\HelloVirus\test1.bat
+echo goto main >> C:\Users\%username%\Desktop\HelloVirus\test1.bat
+start C:\Users\%username%\Desktop\HelloVirus\test1.bat
+echo [%passcode% Complete]
 pause
 goto main
+
+:specialdestroy
+echo @echo off >> C:\Users\%username%\Desktop\HelloVirus\test2.bat
+echo :main >> C:\Users\%username%\Desktop\HelloVirus\test2.bat
+echo echo Starting SpecialDestroy... >> C:\Users\%username%\Desktop\HelloVirus\test2.bat
+echo goto main >> C:\Users\%username%\Desktop\HelloVirus\test2.bat
+start C:\Users\%username%\Desktop\HelloVirus\test2.bat
+echo [%passcode% Complete]
+pause
+goto main
+
 :help
 echo help
 pause
