e99b8b4f0d79617f00ab00d350c9c3266680e1ed
Thang Nguyen <118606753+VThang51@users.noreply.github.com>
2024-01-16 13:10:18 +0700

    Update HelloVirus.bat

--- HelloVirus.bat
+++ HelloVirus.bat
@@ -2,29 +2,27 @@
 :main
 cls
 title Virus Activation
-mode con:cols=46 lines=14
-echo "############################################"
-echo "##   _   _ _____ _     _           ___    ##"
-echo "##  | | | | ____| |   | |         / _ \   ##"
-echo "##  | |_| |  _| | |   | |   _____| | | |  ##"
-echo "##  |  _  | |___| |___| |__|_____| |_| |  ##"
-echo "##  |_| |_|___ _|_ ___|_____| __  \___/_  ##"
-echo "##            | | / /  _/ _ \/ / / / __/  ##"
-echo "##            | |/ // // , _/ /_/ /\ \    ##"
-echo "##            |___/___/_/|_|\____/___/    ##"
-echo "##                                        ##"
-echo "############################################"
+mode con:cols=44 lines=14
+echo ############################################
+echo ##   _   _ _____ _     _           ___    ##
+echo ##  ^| ^| ^| ^| ____^| ^|   ^| ^|         / _ \   ##
+echo ##  ^| ^|_^| ^|  _^| ^| ^|   ^| ^|   _____^| ^| ^| ^|  ##
+echo ##  ^|  _  ^| ^|___^| ^|___^| ^|__^|_____^| ^|_^| ^|  ##
+echo ##  ^|_^| ^|_^|___ _^|_ ___^|_____^| __  \___/_  ##
+echo ##            ^| ^| / /  _/ _ \/ / / / __/  ##
+echo ##            ^| ^|/ // // , _/ /_/ /\ \    ##
+echo ##            ^|___/___/_/^|_^|\____/___/    ##
+echo ##                                        ##
+echo ############################################
 set passcode=
 set /p passcode=Passcode: 
-if '%passcode%'=='DestroyMe' goto destroyme
-if '%passcode%'=='SpecialDestroy' goto specialdestroy
-if '%passcode%'=='Help' goto help
-if '%passcode%'=='About' goto about
+if '%passcode%'=='001' goto 001
+if '%passcode%'=='002' goto 002
 echo [Wrong Passcode]
 pause
 goto main
 
-:destroyme
+:001
 echo @echo off >> C:\Users\%username%\Desktop\HelloVirus\test1.bat
 echo :main >> C:\Users\%username%\Desktop\HelloVirus\test1.bat
 echo echo Starting DestroyMe... >> C:\Users\%username%\Desktop\HelloVirus\test1.bat
@@ -34,17 +32,12 @@ echo [%passcode% Complete]
 pause
 goto main
 
-:specialdestroy
+:002
 echo @echo off >> C:\Users\%username%\Desktop\HelloVirus\test2.bat
 echo :main >> C:\Users\%username%\Desktop\HelloVirus\test2.bat
 echo echo Starting SpecialDestroy... >> C:\Users\%username%\Desktop\HelloVirus\test2.bat
 echo goto main >> C:\Users\%username%\Desktop\HelloVirus\test2.bat
 start C:\Users\%username%\Desktop\HelloVirus\test2.bat
 echo [%passcode% Complete]
-pause
-goto main
-
-:help
-echo help
 pause
 goto main
